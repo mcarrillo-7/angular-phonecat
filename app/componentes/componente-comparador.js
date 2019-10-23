e814897d5d1b24ea6330042dfa445e77ca828f49
Curso <manu.carrilloortega@gmail.com>
2019-10-23 09:53:12 +0200

    componente comparador parte 2

---
+++ app/componentes/componente-comparador.js
@@ -0,0 +1,32 @@
+angular.
+  module('phoneComparador').
+  component('componenteComparador', {
+    templateUrl: '../phone-comparador/phone-comparador.template.html',
+    controller: ['Phone',
+      function PhoneComparadorController(Phone) {        
+
+        console.trace('PhoneComparadorController');
+
+        var self = this;
+
+        self.phones = Phone.query();
+        self.phone1 = {};
+        self.phone2 = {};
+        self.orderProp = 'age';
+
+        this.selecionar = function(phone){
+          console.trace('seleccionado movil');
+        
+          if(self.phone1.length<1){
+            self.phone1 = phone;
+          }else{
+            self.phone2 = self.phone1;
+            self.phone1 = phone;
+          }
+        }
+
+
+
+      }
+    ]
+  });
